1021e3ed880bb27240bf1e8d83ffc512584b5001
noorfatima <noorfatima2830@gmail.com>
2023-12-07 12:03:24 +0500

    module function

--- index.js
+++ index.js
@@ -25,6 +25,10 @@
  function multi2(x,y,z,a){
     return x*y*z*a;
  }
+ function mod(x,y){
+    return x%y;
+ }
+ console.log(mod(2,3));
  console.log(multi2(2,3,2,4));
  console.log(multi1(2,3,2));
  console.log(multi(2,3));
