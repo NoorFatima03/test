f17c3ca7a80ebd3f05f88e6c96e6007f8b10a0dc
noorfatima <noorfatima2830@gmail.com>
2023-12-07 11:48:05 +0500

    sum function with 4 parameters

--- index.js
+++ index.js
@@ -7,5 +7,9 @@
  function sum1(x,y,z){
     return x+y+z;
  }
+ function sum2(x,y,z,a){
+    return x+y+z+a;
+ }
+ console.log(sum2(2,3,11,12));
  console.log(sum(2,3,11));
   console.log(sum1(2,3,11));
