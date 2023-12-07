4fffa9d119dd50348f05f3d13d30b9ab982534e1
noorfatima <noorfatima2830@gmail.com>
2023-12-07 11:54:03 +0500

    minus function with 3 parameters

--- index.js
+++ index.js
@@ -13,7 +13,11 @@
  function minus(x,y){
     return x-y;
  }
- console.log(minus(2,3,));
+ function minus1(x,y,z){
+    return x-y-z;
+ }
+ console.log(minus1(2,3,4));
+ console.log(minus(2,3));
  console.log(sum2(2,3,11,12));
  console.log(sum(2,3,11));
   console.log(sum1(2,3,11));
