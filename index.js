36bdb62e625b510ec6c3ccd375cdcbaa467b5668
noorfatima <noorfatima2830@gmail.com>
2023-12-07 13:55:28 +0500

    changing position

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+function table3(x){
+   let sum = 0;
+   for(let i =0;i<=10;i++){
+        sum = x*i;
+        console.log(x+"*"+i+"="+sum)
+   }
+ }
+ table3(3);
  function sum(x,y){
     return x+y;
  }
@@ -41,11 +49,4 @@
  console.log(sum(2,3,11));
   console.log(sum1(2,3,11));
 
-  function table3(x){
-    let sum = 0;
-    for(let i =0;i<=10;i++){
-         sum = x*i;
-         console.log(x+"*"+i+"="+sum)
-    }
-  }
-  table3(3);
+  
